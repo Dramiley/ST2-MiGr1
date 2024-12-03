a757a3e7df8cdb0d6c9a5747097d092b3537c730
Dramiley <MorgensternRobin04@gmail.com>
2024-12-03 14:22:41 +0100

    enthält errors (also tests starten nicht)

--- st2migr1/src/test/java/com/st2e3/ElementTest.java
+++ st2migr1/src/test/java/com/st2e3/ElementTest.java
@@ -3,5 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ElementTest {
-    
+    @Test
+    public void test() {
+        // Add your test code here
+    }
 }
--- st2migr1/src/test/java/com/st2e3/SimpleLinkedListTest.java
+++ st2migr1/src/test/java/com/st2e3/SimpleLinkedListTest.java
@@ -6,4 +6,59 @@ import java.beans.Transient;
 import org.junit.jupiter.api.Test;
 
 public class SimpleLinkedListTest {
+    @Test
+    public void testAdd() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertEquals(3, list.size());
+    }
+
+    @Test
+    public void testSize() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        assertEquals(0, list.size());
+        list.add(1);
+        assertEquals(1, list.size());
+        list.add(2);
+        assertEquals(2, list.size());
+        list.add(3);
+        assertEquals(3, list.size());
+    }
+
+    @Test
+    public void testIterator() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        var it = list.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(1, it.next());
+        assertTrue(it.hasNext());
+        assertEquals(2, it.next());
+        assertTrue(it.hasNext());
+        assertEquals(3, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void testIteratorEmpty() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        var it = list.iterator();
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void testIteratorOneElement() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        var it = list.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(1, it.next());
+        assertFalse(it.hasNext());
+    }
+
+    
 }
