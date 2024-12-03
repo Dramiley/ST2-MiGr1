6d257a5bc0ee8fa983b16d9c3d73f6418f515772
emmssp <emma.spllr@gmail.com>
2024-12-03 12:57:51 +0100

    fixed pom

---
+++ st2migr1/src/test/java/com/st2e3/SimpleLinkedListTest.java
@@ -0,0 +1,9 @@
+package test.java.com.st2e3;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleLinkedListTest {
+}
